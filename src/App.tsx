ddfd6031c69bfbbe24af8f0ed33aae76739b8fb3
T31K <t31kmunwong@gmail.com>
2023-06-17 22:19:56 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,32 @@ import { listenNowAlbums, madeForYouAlbums } from "./data/albums"
 import { playlists } from "./data/playlists"
 import { PlusCircle } from "lucide-react"
 
-import "./styles/styles.css"
+import "./styles/styles.scss"
 
+import Home from "./pages/Home"
 import { Button } from "./components/ui/button"
 
-
-export default function MusicPage() {
+function App() {
   return (
     <>
-      <div className="flex w-screen h-screen text-gray-700">
-
-
-          <div className="flex flex-col w-64 overflow-hidden border-r">
-
+      <div className="flex w-screen h-screen text-gray-700 overflow-hidden">
+        <div className="flex flex-col w-64 !min-w-[16rem] !max-w-[16rem] overflow-hidden border-r">
           <Sidebar playlists={playlists}  />
-
+        </div>
+        <div className="flex flex-col flex-grow overflow-y-auto">
+          <div className="flex-grow p-4">
+            <div className="flex items-center flex-col">
+              <Home />
+            </div>
           </div>
-          <div className="flex flex-col flex-grow">
-       
-              <div className="flex-grow overflow-auto ">
-           
-
-              </div>
-          </div>
+        </div>
+        <div className="flex flex-col w-64 !min-w-[16rem] !max-w-[16rem] overflow-hidden border-l">
+        <Sidebar playlists={playlists}  />
+        </div>
       </div>
-
-
     </>
   )
 }
+
+
+export default App;
