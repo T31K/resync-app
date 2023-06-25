6be3d17c8ff0e0100a6e942bce71b7c5d4901139
T31K <t31kmunwong@gmail.com>
2023-06-25 22:51:26 +0800

    add vercel configs

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
